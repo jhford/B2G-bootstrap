ca3a3ca00f82f7394451a83a90d9396f32a7c255
John Ford <john@johnford.info>
2012-10-08 15:32:16 -0700

    remove firmware extraction from config.sh

--- config-device.sh
+++ config-device.sh
@@ -22,37 +22,30 @@ echo DEVICE_NAME=$1 >> .tmp-config
 case "$1" in
 "galaxy-s2")
 	echo DEVICE=galaxys2 >> .tmp-config &&
-	(cd device/samsung/galaxys2 && ./extract-files.sh)
 	;;
 
 "galaxy-nexus")
 	echo DEVICE=maguro >> .tmp-config &&
-	(cd device/samsung/maguro && ./download-blobs.sh)
 	;;
 
 "optimus-l5")
 	echo DEVICE=m4 >> .tmp-config &&
-	(cd device/lge/m4 && ./extract-files.sh)
 	;;
 
 "nexus-s")
 	echo DEVICE=crespo >> .tmp-config &&
-	(cd device/samsung/crespo && ./download-blobs.sh)
 	;;
 
 "nexus-s-4g")
 	echo DEVICE=crespo4g >> .tmp-config &&
-	(cd device/samsung/crespo4g && ./download-blobs.sh)
 	;;
 
 "otoro"|"unagi")
 	echo DEVICE=$1 >> .tmp-config &&
-	(cd device/qcom/$1 && ./extract-files.sh)
 	;;
 
 "pandaboard")
 	echo DEVICE=panda >> .tmp-config &&
-	(cd device/ti/panda && ./download-blobs.sh)
 	;;
 
 "emulator")
